41f06c7adb2ec2d1c06282ae6574f077be4fda6e
Documenter.jl <documenter@juliadocs.github.io>
2026-01-31 14:51:08 +0000

    build based on fe64668

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"manual/#Manual","page":"Manual","title":"Manual","text":"Content coming soon.","category":"section"},{"location":"#PsiTK.jl","page":"Home","title":"PsiTK.jl","text":"Welcome to the PsiTK documentation!","category":"section"},{"location":"#API-Reference","page":"Home","title":"API Reference","text":"```@autodocs Modules = [PsiTK]","category":"section"}]
+}
